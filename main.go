79f6b6e0a7ecb8c649cb6aac97186a74492c3303
Joerg Mueller <joergm42@gmail.com>
2014-11-24 17:59:56 +0100

    Generierung der Schedule als html.

--- main.go
+++ main.go
@@ -3,38 +3,46 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"html/template"
 	"os"
-	"text/template"
 )
 
 type entry struct {
-	Timestamp string
 	Name      string
 	EMail     string
 	Telephone string
 	Language  string
+	Day       string
+	Track     string
+	Time      string
 	Status    string
 	Confirmed string
+	Title     string
 	Abstract  string
 	Bio       string
+	Remarks   string
 }
 
 func recordToStruct(record []string) entry {
 
-	fullsizeArray := make([]string, 9) //prevent index out of bounds
+	fullsizeArray := make([]string, 13) //prevent index out of bounds
 	copy(fullsizeArray, record)
 
 	e := entry{}
 
-	e.Timestamp = fullsizeArray[0]
-	e.Name = fullsizeArray[1]
-	e.EMail = fullsizeArray[2]
-	e.Telephone = fullsizeArray[3]
-	e.Language = fullsizeArray[4]
-	e.Status = fullsizeArray[5]
-	e.Confirmed = fullsizeArray[6]
-	e.Abstract = fullsizeArray[7]
-	e.Bio = fullsizeArray[8]
+	e.Name = fullsizeArray[0]
+	e.EMail = fullsizeArray[1]
+	e.Telephone = fullsizeArray[2]
+	e.Language = fullsizeArray[3]
+	e.Day = fullsizeArray[4]
+	e.Track = fullsizeArray[5]
+	e.Time = fullsizeArray[6]
+	e.Status = fullsizeArray[7]
+	e.Confirmed = fullsizeArray[8]
+	e.Title = fullsizeArray[9]
+	e.Abstract = fullsizeArray[10]
+	e.Bio = fullsizeArray[11]
+	e.Remarks = fullsizeArray[12]
 	return e
 }
 
@@ -56,11 +64,13 @@ func main() {
 
 	t, _ := template.ParseFiles("schedule-template.ctmpl")
 
+	fmt.Printf("<!-- START day %s track %s -->\n", os.Args[2], os.Args[3])
 	for _, line := range content {
 		record := recordToStruct(line)
-		if record.Status == "1" {
+		if record.Day == os.Args[2] && record.Track == os.Args[3] {
 			t.Execute(os.Stdout, record)
 		}
 	}
+	fmt.Printf("<!-- END day %s track %s -->\n", os.Args[2], os.Args[3])
 
 }
